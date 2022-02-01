47d5a9ceaf46e08ce277f4611759292a4cb828b6
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2022-02-01 14:03:15 +0100

    feat(ci): adding the VPP-UPF tutorial to pipeline
    Signed-off-by: Raphael Defosseux <raphael.defosseux@eurecom.fr>

--- ci-scripts/checkTutorialHtmlReport.py
+++ ci-scripts/checkTutorialHtmlReport.py
@@ -40,7 +40,7 @@ class HtmlReport():
 		self.file = open(cwd + '/test_results_oai_cn5g_tutorials.html', 'w')
 		self.generateHeader()
 
-		tutorials = ['static-ue-ip']
+		tutorials = ['static-ue-ip', 'vpp-upf-gnbsim']
 		for tutorial in tutorials:
 			if not os.path.isfile(cwd + '/archives/' + tutorial + '.log'):
 				continue
