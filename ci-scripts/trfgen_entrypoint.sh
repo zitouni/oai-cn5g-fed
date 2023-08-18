1c79079cccd2ea355c4f07ec292e1bcdaf85cf82
Rohan <rohan.kharade@openairinterface.org>
2023-08-18 11:44:25 +0000

    Fix docker-compose for trfgen

--- ci-scripts/trfgen_entrypoint.sh
+++ ci-scripts/trfgen_entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-UPF_HOST=${UPF_HOST:-oai-upf}
+UPF_FQDN=${UPF_FQDN:-oai-upf}
 USE_FQDN=${USE_FQDN:-no}
 UE_NETWORK=${UE_NETWORK:-12.1.1.0/24}
 
 if [[ ${USE_FQDN} == "yes" ]];then
-    UPF_ADDR=(`getent hosts oai-upf | awk '{print $1}'`)
+    UPF_ADDR=(`getent hosts $UPF_FQDN | awk '{print $1}'`)
     echo -e "\nResolving UPF by FQDN : $UPF_FQDN - $UPF_ADDR"
     ip route add $UE_NETWORK via $UPF_ADDR dev eth0
 fi
