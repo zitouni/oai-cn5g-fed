4571636382cb3986f7233fb69d2a3f0de1b17b25
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2023-11-16 13:00:14 +0100

    fix(ci): automation issues fixes   *  basic entrypoint for ext-dn image implies  that the UPF shall be deployed before EXT-DN      -  solution: loop to wait for UPF to be resolvable   *  In core-network.py, all containers IP addresses are hard-coded      -  solution: method that reads docker-compose file(s) and retrieves containers IP address   *  in core-network.py when we start a capture, we have based our capture filter on a fixed IP address on ext-dn      -  solution: better capture filter
    Signed-off-by: Raphael Defosseux <raphael.defosseux@eurecom.fr>

--- ci-scripts/trfgen_entrypoint.sh
+++ ci-scripts/trfgen_entrypoint.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 UPF_FQDN=${UPF_FQDN:-oai-upf}
 USE_FQDN=${USE_FQDN:-no}
 UE_NETWORK=${UE_NETWORK:-12.1.1.0/24}
 
 if [[ ${USE_FQDN} == "yes" ]];then
+    echo -e "Trying to resolve UPF by FQDN : $UPF_FQDN"
+    x=0
+    while [ $x -le 50 ]
+    do
+        echo -e "Try number $x"
+        getent hosts $UPF_FQDN > /dev/null
+        ret=$?
+        if [[ $ret -eq 0 ]]; then
+            x=100
+        else
+            x=$((x + 1))
+            sleep 5
+        fi
+    done
+    if [[ $ret -ne 0 ]]; then
+      echo -e "Could not resolve $UPF_FQDN"
+      exit 2
+    fi
     UPF_ADDR=(`getent hosts $UPF_FQDN | awk '{print $1}'`)
     echo -e "\nResolving UPF by FQDN : $UPF_FQDN - $UPF_ADDR"
+    echo -e "ip route add $UE_NETWORK via $UPF_ADDR dev eth0"
     ip route add $UE_NETWORK via $UPF_ADDR dev eth0
 fi
 
